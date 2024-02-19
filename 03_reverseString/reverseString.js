fd7bffa6b5975fc37c5e12be11026a92569a48c9
Adam <14831693+adambreese@users.noreply.github.com>
2024-02-19 18:32:26 -0500

    Pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    reversed = '';
+    word = string;
+    for (i = word.length-1; i >= 0; i--) {
+        reversed = reversed + word.charAt(i);
+        console.log(reversed);
+    }
+    return reversed;
 };
 
 // Do not edit below this line
