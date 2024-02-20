89a91f756e36cc5c666f66de713ca100e5f10f8b
Adam <14831693+adambreese@users.noreply.github.com>
2024-02-19 19:38:08 -0500

    Pass 8 out of 8 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+    newLength = array.length
+    for (i = 0; i < newLength; i++) {
+        for (j = 0; j < toRemove.length; j++) {
+            if ((array.at(i) == toRemove.at(j)) && typeof(array.at(i)) == typeof(toRemove.at(j))) {
+                array.splice(i, 1);
+                newLength--;
+                i--;
+                console.log(array);
+            }        
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
