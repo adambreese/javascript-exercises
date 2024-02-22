938d3f66b1d805c8a8ab39ad3891c13c331b5d5a
Adam <14831693+adambreese@users.noreply.github.com>
2024-02-22 16:12:57 -0500

    Pass all calculator tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,24 +3,34 @@ const add = function(num1, num2) {
   return addThese.reduce((total, currentItem) => total + currentItem, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  const subtractThese = [num1, num2];
+  return subtractThese.reduce((total, currentItem) => total - currentItem);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, currentItem) => total + currentItem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, currentItem) => total * currentItem, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let powerUp = num;
+  while (power-1) {
+    powerUp = powerUp * num;
+    power--;
+  }
+  return powerUp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = Array(num);
+  for (i = num; i > 0; i--){
+    fact[num-i] = i;
+  }
+  return multiply(fact);
 };
 
 // Do not edit below this line
