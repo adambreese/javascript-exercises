05e119a3a06f161bc5120353179e59b1250e0770
Adam <14831693+adambreese@users.noreply.github.com>
2024-02-22 17:42:37 -0500

    Why bother filtering to include numbers, not valuable learning right now

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let check = Array.from(string.toUpperCase());
+    console.log(check);
+    // filter for letters
+    const filtered = check.filter((letter) => letter.toLowerCase() != letter.toUpperCase());
+    console.log(filtered);
+    // does reverse = string?
+    let reversed = filtered.slice();
+    reversed.reverse();
 
+    return (reversed.every((value, index) => value === filtered[index]));
 };
 
 // Do not edit below this line
