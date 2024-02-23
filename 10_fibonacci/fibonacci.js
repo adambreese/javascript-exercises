dde360167b4873cc9b260350d6bb11926f4d7631
Adam <14831693+adambreese@users.noreply.github.com>
2024-02-23 13:53:54 -0500

    Pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    
+    if (num >= 0) {
+        const sequence = Array(num);
+        let lastItem = 0;
+        let secondToLast = 1;
+        for (i = 0; i < num; i++) {
+            sequence[i] = lastItem + secondToLast;
+            secondToLast = lastItem;
+            lastItem = sequence[i];
+        }
+        return lastItem;
+    }
+    else {
+        return "OOPS";
+    }
+    
 };
 
 // Do not edit below this line
