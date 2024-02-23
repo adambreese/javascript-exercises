70ec310fa76d8c0d046e71ab9109fb706aa1daf2
Adam <14831693+adambreese@users.noreply.github.com>
2024-02-23 14:09:51 -0500

    Pass first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const sorted = people.sort((a, b) =>
+    (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)
+    ? 1 : -1);
+    return sorted[sorted.length-1];
 };
 
 // Do not edit below this line
